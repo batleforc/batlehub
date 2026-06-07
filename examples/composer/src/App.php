945dc8e3d230167b5e88be0498dd9315f348400c
Max Batleforc <maxleriche.60@gmail.com>
2026-06-07 15:18:04 +0200

    feat: wip

--- examples/composer/src/App.php
+++ examples/composer/src/App.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+#[AsCommand(name: 'app:hello', description: 'Prints a greeting')]
 class HelloCommand extends Command
 {
-    protected static string $defaultName = 'app:hello';
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('<info>Hello from my-app!</info>');
