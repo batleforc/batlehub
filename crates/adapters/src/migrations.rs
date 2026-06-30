dad5f64019ca58c9bb69b1ba7fc04ab90d70a097
Max Batleforc <maxleriche.60@gmail.com>
2026-06-30 21:01:27 +0000

    feat: wip
    Signed-off-by: Max Batleforc <maxleriche.60@gmail.com>

--- crates/adapters/src/migrations.rs
+++ crates/adapters/src/migrations.rs
@@ -22,96 +22,96 @@ macro_rules! mig {
 /// Build the embedded SQL migrator without connecting to a database.
 pub fn embedded_migrator() -> Migrator {
     Migrator::with_migrations(vec![
-            mig!(1, "init", "../migrations/001_init.sql"),
-            mig!(
-                2,
-                "artifact storage",
-                "../migrations/002_artifact_storage.sql"
-            ),
-            mig!(3, "user tokens", "../migrations/003_user_tokens.sql"),
-            mig!(
-                4,
-                "artifact size bytes",
-                "../migrations/004_artifact_size_bytes.sql"
-            ),
-            mig!(5, "metadata cache", "../migrations/005_metadata_cache.sql"),
-            mig!(6, "local packages", "../migrations/006_local_packages.sql"),
-            mig!(
-                7,
-                "artifact cache meta",
-                "../migrations/007_artifact_cache_meta.sql"
-            ),
-            mig!(8, "quota", "../migrations/008_quota.sql"),
-            mig!(
-                9,
-                "local packages status",
-                "../migrations/009_local_packages_status.sql"
-            ),
-            mig!(10, "rate limit", "../migrations/010_rate_limit.sql"),
-            mig!(
-                11,
-                "package ownership",
-                "../migrations/011_package_ownership.sql"
-            ),
-            mig!(12, "signing", "../migrations/012_signing.sql"),
-            mig!(13, "beta channel", "../migrations/013_beta_channel.sql"),
-            mig!(14, "ip blocks", "../migrations/014_ip_blocks.sql"),
-            mig!(
-                15,
-                "team namespaces",
-                "../migrations/015_team_namespaces.sql"
-            ),
-            mig!(
-                16,
-                "package visibility",
-                "../migrations/016_package_visibility.sql"
-            ),
-            mig!(
-                17,
-                "access events indexes",
-                "../migrations/017_access_events_idx.sql"
-            ),
-            mig!(18, "config changes", "../migrations/018_config_changes.sql"),
-            mig!(19, "system kv", "../migrations/019_system_kv.sql"),
-            mig!(20, "artifact sboms", "../migrations/020_artifact_sboms.sql"),
-            mig!(
-                21,
-                "access events covering idx",
-                "../migrations/021_access_events_covering_idx.sql"
-            ),
-            mig!(
-                22,
-                "notification subscriptions",
-                "../migrations/022_notification_subscriptions.sql"
-            ),
-            mig!(
-                23,
-                "inbound webhook events",
-                "../migrations/023_inbound_webhook_events.sql"
-            ),
-            mig!(
-                24,
-                "notification subscriptions gin index",
-                "../migrations/024_notification_subs_gin_index.sql"
-            ),
-            mig!(
-                25,
-                "artifact vulnerabilities",
-                "../migrations/025_artifact_vulnerabilities.sql"
-            ),
-            mig!(
-                26,
-                "artifact cache checksum",
-                "../migrations/026_artifact_cache_checksum.sql"
-            ),
-            mig!(
-                27,
-                "deprecation unlisting",
-                "../migrations/027_deprecation_unlisting.sql"
-            ),
-            mig!(28, "user blocks", "../migrations/028_user_blocks.sql"),
-            mig!(29, "audit ip ua", "../migrations/029_audit_ip_ua.sql"),
-        ])
+        mig!(1, "init", "../migrations/001_init.sql"),
+        mig!(
+            2,
+            "artifact storage",
+            "../migrations/002_artifact_storage.sql"
+        ),
+        mig!(3, "user tokens", "../migrations/003_user_tokens.sql"),
+        mig!(
+            4,
+            "artifact size bytes",
+            "../migrations/004_artifact_size_bytes.sql"
+        ),
+        mig!(5, "metadata cache", "../migrations/005_metadata_cache.sql"),
+        mig!(6, "local packages", "../migrations/006_local_packages.sql"),
+        mig!(
+            7,
+            "artifact cache meta",
+            "../migrations/007_artifact_cache_meta.sql"
+        ),
+        mig!(8, "quota", "../migrations/008_quota.sql"),
+        mig!(
+            9,
+            "local packages status",
+            "../migrations/009_local_packages_status.sql"
+        ),
+        mig!(10, "rate limit", "../migrations/010_rate_limit.sql"),
+        mig!(
+            11,
+            "package ownership",
+            "../migrations/011_package_ownership.sql"
+        ),
+        mig!(12, "signing", "../migrations/012_signing.sql"),
+        mig!(13, "beta channel", "../migrations/013_beta_channel.sql"),
+        mig!(14, "ip blocks", "../migrations/014_ip_blocks.sql"),
+        mig!(
+            15,
+            "team namespaces",
+            "../migrations/015_team_namespaces.sql"
+        ),
+        mig!(
+            16,
+            "package visibility",
+            "../migrations/016_package_visibility.sql"
+        ),
+        mig!(
+            17,
+            "access events indexes",
+            "../migrations/017_access_events_idx.sql"
+        ),
+        mig!(18, "config changes", "../migrations/018_config_changes.sql"),
+        mig!(19, "system kv", "../migrations/019_system_kv.sql"),
+        mig!(20, "artifact sboms", "../migrations/020_artifact_sboms.sql"),
+        mig!(
+            21,
+            "access events covering idx",
+            "../migrations/021_access_events_covering_idx.sql"
+        ),
+        mig!(
+            22,
+            "notification subscriptions",
+            "../migrations/022_notification_subscriptions.sql"
+        ),
+        mig!(
+            23,
+            "inbound webhook events",
+            "../migrations/023_inbound_webhook_events.sql"
+        ),
+        mig!(
+            24,
+            "notification subscriptions gin index",
+            "../migrations/024_notification_subs_gin_index.sql"
+        ),
+        mig!(
+            25,
+            "artifact vulnerabilities",
+            "../migrations/025_artifact_vulnerabilities.sql"
+        ),
+        mig!(
+            26,
+            "artifact cache checksum",
+            "../migrations/026_artifact_cache_checksum.sql"
+        ),
+        mig!(
+            27,
+            "deprecation unlisting",
+            "../migrations/027_deprecation_unlisting.sql"
+        ),
+        mig!(28, "user blocks", "../migrations/028_user_blocks.sql"),
+        mig!(29, "audit ip ua", "../migrations/029_audit_ip_ua.sql"),
+    ])
 }
 
 #[cfg(test)]
